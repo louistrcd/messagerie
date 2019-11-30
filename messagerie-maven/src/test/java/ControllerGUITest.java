ac63e47e37941b8779623c946fdb0014f41195a2
louistrcd <louis.tracadas@free.fr>
2019-11-30 08:38:28 +0100

    ajout version maven

---
+++ messagerie-maven/src/test/java/ControllerGUITest.java
@@ -0,0 +1,19 @@
+import java.rmi.RemoteException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+
+import application.ControllerGUI;
+
+
+public class ControllerGUITest {
+
+	@Test
+	public void testController() throws RemoteException {
+		//Create the searchQuery object
+		ControllerGUI controller = mock(ControllerGUI.class);
+		
+	}
+	
+}
